322143df800eb4677b99f0d21ec4e2ec9578f6f1
Zygmunt Krynicki <me@zygoon.pl>
2018-08-10 00:17:50 +0200

    Add a "goodbye" app
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- hello-fedora.c
+++ hello-fedora.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	printf("Hello Fedora!\n");
+	if (argc == 2 && strcmp(argv[1], "--bye") == 0) {
+		printf("Goodbye Fedora\n");
+	} else {
+		printf("Hello Fedora!\n");
+	}
 	return 0;
 }
